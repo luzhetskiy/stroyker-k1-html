51b16f642bfc08f68987fe9cafcd57184648df09
KarenAvetisyan <73932421+KarenAvetisyan@users.noreply.github.com>
2023-04-24 14:26:55 +0400

    Add files via upload

--- components_page/scripts/header-global-mobile.js
+++ components_page/scripts/header-global-mobile.js
@@ -21,7 +21,7 @@ $(".close_mm_menu").click(function() {
                                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
                                         <rect x="1.41406" width="17" height="2" rx="1" transform="rotate(45 1.41406 0)" fill="white"/>
                                         <rect y="12.0208" width="17" height="2" rx="1" transform="rotate(-45 0 12.0208)" fill="white"/>
-                                        </svg>'></button>`);
+                                        </svg></button>`);
                                      
 }addButton();
 
