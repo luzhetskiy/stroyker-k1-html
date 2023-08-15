419569a8b642a089afb4e534db5cd06c896256d6
unknown <nikitos.gg.yt@gmail.com>
2023-08-15 08:39:47 +0400

    feat: default animations

--- components_page/scripts/defaultAnimation.js
+++ components_page/scripts/defaultAnimation.js
@@ -1,26 +1,47 @@
 $(document).ready(function () {
-  /*
-    Пример назначения анимации:
-    $() - функция для выборки элементов по ccs селектору, принимает аргументом строку 
-    .attr({'атрибут': 'значение'}) - метод позволяющий задать нужные атрибуты для элементов которые были найдены
-    все типы опций для анимаций можно найти по ссылке https://michalsnik.github.io/aos/
-  */
-  // $(".class-name") // поиск по css селектору элемента/элементов где должна быть анимация
-  // .attr({ // метод назначающий атрибуты
-  //   "data-aos": "fade", // тип анимации: fade
-  //   "data-aos-delay": "100", // задержка до начала анимации в миллисекундах: 100
-  //   "data-aos-duration": "1000", // продолжительность анимации в миллисекундах: 1000
-  // });
 
-  /* 
-    Пример для анимации заголовков h1,
-    с типом анимации fade,
-    задержкой 100ms
-    длительностью 1000ms:
-  */
-  // $("h1").attr({
-  //   "data-aos": "fade",
-  //   "data-aos-delay": "100",
-  //   "data-aos-duration": "1000",
-  // });
+  // Пример анимации для заголовков h1:
+  $("h1").attr({
+    "data-aos": "fade", //тип анимации 
+    "data-aos-delay": "100", // задержка
+    "data-aos-duration": "1000", // продолжительность
+  });
+  
+  // анимация слайда
+  $(".slide-content").attr({
+    "data-aos": "fade",
+    "data-aos-delay": "100",
+    "data-aos-duration": "1000",
+  });
+  // анимация больших слайдов
+  $(".owl-item").attr({
+    "data-aos": "fade",
+    "data-aos-delay": "100",
+    "data-aos-duration": "1000",
+  });
+  // анимация карточек продуктов
+  $(".product-item").attr({
+    "data-aos": "fade",
+    "data-aos-delay": "100",
+    "data-aos-duration": "1000",
+  });
+  // анимация карточек продуктов v2
+  $(".product-card-v2").attr({
+    "data-aos": "fade",
+    "data-aos-delay": "100",
+    "data-aos-duration": "1000",
+  });
+  // анимация контента банера
+  $(".content-standard-box").attr({
+    "data-aos": "fade",
+    "data-aos-delay": "100",
+    "data-aos-duration": "1000",
+  });
+  // анимация формы в подвале
+  $(".question-form").attr({
+    "data-aos": "fade",
+    "data-aos-delay": "100",
+    "data-aos-duration": "1000",
+  });
+
 });
